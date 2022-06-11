9cabd8d6ed4415c8f7561edea00451862eb2c5c0
michael <89133482@qq.com>
2022-06-11 09:42:17 +0800

    make threadpool resizable

--- ThreadPool.h
+++ ThreadPool.h
@@ -14,48 +14,73 @@
 class ThreadPool {
 public:
     ThreadPool(size_t);
+
     template<class F, class... Args>
     auto enqueue(F&& f, Args&&... args) 
         -> std::future<typename std::result_of<F(Args...)>::type>;
+
+    void stopThreads();
+
+    int  resize(int threads); 
+
+    int  getThreadNum();
+
     ~ThreadPool();
 private:
+    void newWorker(int i);
+
     // need to keep track of threads so we can join them
     std::vector< std::thread > workers;
     // the task queue
     std::queue< std::function<void()> > tasks;
+    std::vector< bool > flags;
+    
     
     // synchronization
     std::mutex queue_mutex;
     std::condition_variable condition;
     bool stop;
+    int  m_threadNum;
 };
  
 // the constructor just launches some amount of workers
 inline ThreadPool::ThreadPool(size_t threads)
     :   stop(false)
 {
+    m_threadNum = threads;
+
+    std::vector< bool > tmp(m_threadNum, false);
+    flags.swap(tmp);
+
     for(size_t i = 0;i<threads;++i)
-        workers.emplace_back(
-            [this]
+        newWorker(i);
+}
+
+void ThreadPool::newWorker(int i)
+{
+    workers.emplace_back(
+        [this, i]
+        {
+            for(;;)
             {
-                for(;;)
+                std::function<void()> task;
+
                 {
-                    std::function<void()> task;
-
-                    {
-                        std::unique_lock<std::mutex> lock(this->queue_mutex);
-                        this->condition.wait(lock,
-                            [this]{ return this->stop || !this->tasks.empty(); });
-                        if(this->stop && this->tasks.empty())
-                            return;
-                        task = std::move(this->tasks.front());
-                        this->tasks.pop();
-                    }
-
-                    task();
+                    std::unique_lock<std::mutex> lock(this->queue_mutex);
+                    this->condition.wait(lock,
+                        [this,i]{ return this->stop || this->flags[i] || !this->tasks.empty(); });
+                    if (this->flags[i]) 
+                        return;
+                    if(this->stop && this->tasks.empty())
+                        return;
+                    task = std::move(this->tasks.front());
+                    this->tasks.pop();
                 }
+
+                task();
             }
-        );
+        }
+    );
 }
 
 // add new work item to the pool
@@ -78,9 +103,71 @@ auto ThreadPool::enqueue(F&& f, Args&&... args)
             throw std::runtime_error("enqueue on stopped ThreadPool");
 
         tasks.emplace([task](){ (*task)(); });
+
     }
     condition.notify_one();
+
+
     return res;
+}
+
+void ThreadPool::stopThreads()
+{
+    stop = true;
+}
+
+int ThreadPool::getThreadNum()
+{
+    return m_threadNum;
+}
+
+int ThreadPool::resize(int threads)
+{
+
+    if (threads == 0) 
+        return -1;
+    if (threads == m_threadNum) 
+        return 0;
+
+    //worker removes
+    std::vector< int > removes;
+
+    {
+        std::unique_lock<std::mutex> lock(this->queue_mutex);
+
+        if (threads > m_threadNum) { 
+
+            std::vector< bool > tmp(m_threadNum, false);
+            flags.swap(tmp);
+            for (int i = m_threadNum; i < threads; i++) { 
+                newWorker(i);
+            }
+
+        } else { 
+            for (int i = threads; i < m_threadNum; i++) { 
+                flags[i] = true;
+                removes.push_back(i);
+            }
+        }
+        m_threadNum = threads;
+    }
+
+    //remove thread
+    std::thread th;
+    if (!removes.empty()) { 
+        condition.notify_all();
+        for (int i = removes.size() - 1; i >= 0 ; i--) { 
+            int j = removes[i];
+            workers[j].join();
+            workers.erase(workers.begin() + j);
+        }
+        removes.clear();
+
+        std::vector< bool > tmp(m_threadNum, false);
+        flags.swap(tmp);
+    }
+    
+    return 0;
 }
 
 // the destructor joins all threads
--- example.cpp
+++ example.cpp
@@ -1,29 +1,58 @@
 #include <iostream>
 #include <vector>
 #include <chrono>
+#include <random>
 
 #include "ThreadPool.h"
 
+using namespace std;
+
+std::mutex g_mutex;
+
 int main()
 {
-    
+    random_device rd;
     ThreadPool pool(4);
     std::vector< std::future<int> > results;
+    int k = 0;
 
-    for(int i = 0; i < 8; ++i) {
+    for (int i = 0; i < 1000000; ++i) {
         results.emplace_back(
-            pool.enqueue([i] {
+            pool.enqueue([i, &k] {
+                {
+                    std::unique_lock<std::mutex> lock(g_mutex);
+                    k++;
+                }
+                /*
                 std::cout << "hello " << i << std::endl;
-                std::this_thread::sleep_for(std::chrono::seconds(1));
+                std::this_thread::sleep_for(std::chrono::seconds(0));
                 std::cout << "world " << i << std::endl;
-                return i*i;
+                */
+                return i;
             })
         );
+
+        if (i % 5 == 0) { 
+            int j = rd() %10;
+            cout << "ok" << j <<endl;
+            pool.resize(j);
+            cout << "pool size :"<< pool.getThreadNum()<<endl;
+        }
     }
 
+
+    /*
+    for (int i = 0; i < 8; ++i) {
+        results.emplace_back(pool.enqueue(func, i, i));
+    }
+    */
+
     for(auto && result: results)
-        std::cout << result.get() << ' ';
+        std::cout << "result :" << result.get() << std::endl;
     std::cout << std::endl;
+    std::cout << "k = " << k << std::endl;
+
     
     return 0;
 }
+
